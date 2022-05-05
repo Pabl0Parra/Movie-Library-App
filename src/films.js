a6aa02560350e186735a45f7311e811bbe2803ca
Pabl0Parra <pabsblue16@gmail.com>
2022-05-05 20:41:48 +0200

    added ex_4

--- src/films.js
+++ src/films.js
@@ -1,12 +1,14 @@
 // Exercise 1: Get the array of all directors.
 function getAllDirectors(array) {
+  // array.map() method to iterate [] & create a new [] w/just the directors
   const ALL_DIRECTORS = array.map((movies) => movies.director);
-  // console.log("EXERCICE 1 ->", result);
+
   return ALL_DIRECTORS;
 }
 
 // Exercise 2: Get the films of a certain director
 function getMoviesFromDirector(array, director) {
+  // array.filter() method to create new array w/just films of a certain director
   const CERTAIN_DIRECTOR_FILMS = array.filter(
     (movies) => movies.director == director
   );
@@ -16,11 +18,11 @@ function getMoviesFromDirector(array, director) {
 
 // Exercise 3: Calculate the average of the films of a given director.
 function moviesAverageOfDirector(array, director) {
-  // 1st -> Array films of a certain director
+  // 1st -> array films of a certain director
   const CERTAIN_DIRECTOR_FILMS = array.filter(
     (movies) => movies.director == director
   );
-  // 2nd
+  // 2nd -> array.reduce() method to find the total of a director´s movies score
   const DIRECTOR_SCORE_AVG =
     CERTAIN_DIRECTOR_FILMS.reduce((total, next) => total + next.score, 0) /
     CERTAIN_DIRECTOR_FILMS.length;
@@ -29,8 +31,21 @@ function moviesAverageOfDirector(array, director) {
 }
 
 // Exercise 4:  Alphabetic order by title
-function orderAlphabetically(array) {}
+function orderAlphabetically(array) {
+  // 1st -> spread operator to clone original array so it doesn´t mutate it if we change the new array
+  const CLONE_MOVIE_ARRAY = [...array];
+  // 2nd -> array.sort() method to classify movies titles from A to Z (aaa, aab, abb, abc, acb...)
+  const MOVIES_BY_ABC_ORDER = CLONE_MOVIE_ARRAY.sort((movieA, movieB) =>
+    // string.localeCompare() method returns a number indicating whether a reference string comes before, or after, or is the same as the given string in sort order.
+    movieA.title.localeCompare(movieB.title)
+  );
+  // 3rd -> Create an array w/just the titles
+  const MOVIES_TITLES = MOVIES_BY_ABC_ORDER.map((movies) => movies.title);
+  // 4th -> array.slice() method to create a new array w/just the first 20 elements
+  const INITIAL_20_TITLES_BY_ABC_ORDER = MOVIES_TITLES.slice(0, 20);
 
+  return INITIAL_20_TITLES_BY_ABC_ORDER;
+}
 // Exercise 5: Order by year, ascending
 function orderByYear() {}
 
